f254036039f2bf4a4ef72e430f0465e2d0e6efcf
Muhammad-Farhan-A <73508487+Muhammad-Farhan-A@users.noreply.github.com>
2021-12-19 10:35:42 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     return ["Hello Hai..!!!"];
 });
 
-$router->get('/data', function () use ($router) {
+$router->post('/data', function () use ($router) {
     $results = app('db')->select("SELECT * FROM pengguna");
     return response()->json($results);
 });
